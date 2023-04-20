9e94e2f513982958c6fde511a3773bd172ef0e16
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-19 22:01:03 -0300

    use databindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task2-angular-databinding';
+  username = '';
+  isButtonDisabled = true;
+
+  clearUsername() {
+    this.username = '';
+  }
 }
